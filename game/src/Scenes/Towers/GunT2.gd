770704fc2d892e5d1414df95d74d1b8cbab9fc10
alortius <anna-alina.bondarets@ucu.edu.ua>
2022-08-14 19:09:42 +0300

    Kill them all

--- game/src/Scenes/Towers/GunT2.gd
+++
@@ -1,1 +0,0 @@
-extends "res://src/Scenes/Towers/Turret.gd"
--- game/src/Scenes/Towers/Turret.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-
-func _physics_process(_delta):
-	turn()
-
-func turn():
-	var enemy_position = get_global_mouse_position()
-	get_node("Turret").look_at(enemy_position)
