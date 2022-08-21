b379ca72230899ee629d541be56cd2667d1988fc
alortius <anna-alina.bondarets@ucu.edu.ua>
2022-08-21 20:11:53 +0300

    Update melon interface

--- kavoons/NinjaT1.gd
+++ kavoons/NinjaT1.gd
@@ -2,14 +2,4 @@ extends Melon
 
 
 func _init():
-	tier_num = 1
-
-	buy_cost = 100
-#	sell_cost = 
-#
-#	attack_damage
-#	attack_speed
-#	projectile_speed
-#
-#	crit_rate
-#	crit_multiplier
+	pass
--- kavoons/src/scenes/towers/Melon.gd
+++ kavoons/src/scenes/towers/Melon.gd
@@ -8,29 +8,54 @@ var tier_num: int
 var buy_cost: int
 var sell_cost: int
 
-var attack_type
-var attack_damage: int
-
-var armor_
+var base_attack_radius: int
+var base_attack_type  # from global enum DAMAGE_TYPES
+var base_attack_damage: int
 
 var attack_speed: float
 var projectile_speed: float
 var miss_rate: float
 
 var crit_rate: float
-var crit_damage_multiplier: int
+var crit_strike_multiplier: int
+
+var armor_reduction_percentage: float
+var armor_reduction_flat: int
+
+var resistence_reduction_percentage: float
+var resistence_reduction_flat: int
+
+var target_priority  # from global enum TARGET_PRIORITY
 
 var last_hit_counter: int = 0
+
+#var passive_abilities: Array = []
+var active_abilities: Array = []
 
 onready var sprite := $MelonSprite
 
 
-func _physics_process(_delta):
+func _init():
+  pass
+
+func _ready():
+  pass
+
+func _process(delta):
+  pass
+
+func _physics_process(delta):
 	rotate_to()
 
 func rotate_to():
 	var enemy_position = get_global_mouse_position()
 	sprite.look_at(enemy_position)
+
+func perform_base_attack():
+	pass
+
+func perform_active_ability():
+	pass
 
 func level_up():
 	pass
