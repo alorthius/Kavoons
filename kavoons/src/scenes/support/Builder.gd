099bda61bcc1ebe98c6b956cd29a2f90faac6a15
alortius <anna-alina.bondarets@ucu.edu.ua>
2022-11-09 18:06:48 +0200

    Start building, add tower preview

--- kavoons/src/scenes/support/Builder.gd
+++ kavoons/src/scenes/support/Builder.gd
@@ -1,15 +1,37 @@
-extends Node
+extends Node2D
 
 class_name Builder
 
+var _is_active: bool = false
+var _is_valid: bool = false
+onready var _tower_preview: Sprite = $UI/HUD/TowerPreview
 
-var chosen_melon
+var build_position: Vector2
+var chosen_melon: String
 
-func choose_melon():
+func _ready():
+	for butt in get_tree().get_nodes_in_group("build_buttons"):
+		butt.connect("pressed", self, "_make_active", [butt.get_name()])
+
+func _process(delta):
+	if _is_active:
+		_update_tower_preview()
+
+func _unhandled_input(event):
 	pass
 
-func place_melon():
+func _make_active(melon: String):
+	_is_active = true
+	_tower_preview.set_preview(melon, get_global_mouse_position())
+
+func _update_tower_preview():
+	_tower_preview.update_preview(get_global_mouse_position())
+
+func _choose_melon():
 	pass
 
-func cancel_building():
+func _place_melon():
+	pass
+
+func _cancel_building():
 	pass
---
+++ kavoons/src/scenes/support/TowerPreview.gd
@@ -0,0 +1,27 @@
+extends Sprite
+# the node is the child of CanvasLayer,
+# because it should be displayed on top of everything
+
+# preload sprites to reuse during building
+var _sprites = {
+	'NinjaMelon': preload('res://assets/towers/ninja_melon/T1.png')
+}
+
+var _preview_alpha_channel: float = 0.5
+var _preview_scale_factor: Vector2 = Vector2(5, 5)
+
+
+func set_preview(tower: String, position: Vector2):
+	self.set_texture(_sprites[tower])
+	self.set_scale(_preview_scale_factor)
+	self.modulate.a = _preview_alpha_channel
+	update_preview(position)
+
+func update_preview(position: Vector2):
+	self.set_position(position)
+
+func calcel_preview():
+	self.set_texture(null)
+
+#func _ready():
+#	set_preview('NinjaMelon', Vector2(500, 500))
