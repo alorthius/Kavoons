9c52c2600fc6964c370be6d547f3bef517a40a64
alortius <anna-alina.bondarets@ucu.edu.ua>
2022-11-17 19:56:02 +0200

    Print error code after connecting a signal

--- kavoons/src/scenes/main/GameScene.gd
+++ kavoons/src/scenes/main/GameScene.gd
@@ -5,8 +5,14 @@ onready var waves_timer: Timer = $WavesTimer
 onready var towers_container: Node = $Towers
 onready var cats_container: Path2D = $Map/Path2D
 
+onready var curr_wave
+
+var _signal_err: int = 0
+
 func _ready():
-	builder.connect("tower_placed", self, "_attach_melon")
+	_signal_err = builder.connect("tower_placed", self, "_attach_melon")
+	if _signal_err != 0:
+		print("GameScene: _reade: connect(): ", _signal_err)
 	waves_timer.start()
 
 
@@ -16,9 +22,16 @@ func _attach_melon(new_tower: Melon):
 
 func _on_WavesTimer_timeout():
 	var _wave = load("res://src/scenes/waves/Wave.tscn").instance()
+	curr_wave = _wave
 	self.add_child(_wave, true)
-	$Wave.connect("spawn_enemy", self, "_on_cat_spawn")
-	$Wave.connect("spawning_ended", self, "_on_spawn_end")
+	
+	_signal_err = curr_wave.connect("spawn_enemy", self, "_on_cat_spawn")
+	if _signal_err != 0:
+		print("GameScene: _on_WavesTimer_timeout: connect(): spawn_enemy: ", _signal_err)
+	
+	_signal_err = curr_wave.connect("spawning_ended", self, "_on_spawn_end")
+	if _signal_err != 0:
+		print("GameScene: _on_WavesTimer_timeout: connect(): spawning_ended: ", _signal_err)
 
 
 func _on_cat_spawn(cat):
