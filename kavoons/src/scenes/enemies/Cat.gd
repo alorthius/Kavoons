d53e49def0032d2bc5dc5ef12fded816ab57fc18
alortius <anna-alina.bondarets@ucu.edu.ua>
2022-11-21 21:46:52 +0200

    Spawn cats with smol horizontal offset and add rotation (looks shitty with current cats but pohui

--- kavoons/src/scenes/enemies/Cat.gd
+++ kavoons/src/scenes/enemies/Cat.gd
@@ -14,11 +14,16 @@ var magical_resistance_percentage: float
 var dodge_rate: float
 
 
+func _ready():
+	v_offset = rand_range(-30, 10)
+
+
 func _physics_process(delta):
 	set_offset(get_offset() + move_speed * delta)
 	
 	if get_unit_offset() >= 1:  # reached the end
 		reached_end()
+
 
 func reached_end():
 	queue_free()
