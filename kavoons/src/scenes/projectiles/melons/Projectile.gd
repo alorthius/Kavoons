d2d5e7f77c8ca358b5a1e7a70f40a382807f336e
alortius <anna-alina.bondarets@ucu.edu.ua>
2022-11-26 00:12:03 +0200

    pampampam

--- kavoons/src/scenes/projectiles/melons/Projectile.gd
+++ kavoons/src/scenes/projectiles/melons/Projectile.gd
@@ -1,10 +1,12 @@
 extends Area2D
 
+## Represent a shooting projectile of a melon
 class_name Projectile
 
 var _speed: float
 var _dmg: int
-var _miss_rate: float
+var _is_miss: bool = false
+
 onready var _target: Cat
 onready var _target_area: Area2D = _target.get_node("Area2D")
 
@@ -12,20 +14,17 @@ func _ready():
 	set_as_toplevel(true)  # move independent from parent node
 	set_scale(Vector2(3.5, 3.5))
 
+# TODO: add misses
+
 func _set_properties(speed: float, damage: int, miss_rate: float, target: Cat):
 	_speed = speed
 	_dmg = damage
-	_miss_rate = miss_rate
 	_target = target
 
 func _physics_process(delta):
-	var delta_angle: float = get_angle_to(_target.position)
-	rotate(delta_angle)
+	# follow the enemy's moving position
+	rotate(get_angle_to(_target.position))
 	global_position += (_target.position - position).normalized() * _speed * delta
-	
-#	if overlaps_area(_target_area):
-#		_target.on_hit(_dmg)
-#		queue_free()
 
 func _on_Projectile_area_entered(area):
 	if area == _target_area:
