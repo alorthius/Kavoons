c519c26400014bf853baf3b4e40d36b3f0ef1596
alortius <anna-alina.bondarets@ucu.edu.ua>
2023-01-21 21:02:15 +0200

    tweens huins

--- kavoons/src/scenes/UI/utility/butts/MyButt.gd
+++ kavoons/src/scenes/UI/utility/butts/MyButt.gd
@@ -1,38 +1,44 @@
 extends TextureButton
 
-export var focus_scale := Vector2(0.1, 0.1)
+export(float, 1) var focus_scale
+export(float, 1) var butt_darkened
+export(float, 1) var icon_darkened
 
 onready var _icon: TextureRect = $Icon
 onready var _tween: Tween = $Tween
 
-onready var _scale_init := rect_scale
-onready var _scale_final := rect_scale + focus_scale
+onready var _scale_init: Vector2 = rect_scale
+onready var _scale_final: Vector2 = rect_scale + focus_scale * Vector2.ONE
 
-onready var _position_init := rect_position
-onready var _position_final := rect_position - rect_size * focus_scale / 2.0
+onready var _position_init: Vector2 = rect_position
+onready var _position_final: Vector2 = rect_position - rect_size * focus_scale * Vector2.ONE / 2.0
 
-var _tween_time: float = 0.05
+onready var _modulate_init := modulate
+onready var _modulate_butt_final := modulate.darkened(butt_darkened)
+onready var _modulate_icon_final := modulate.darkened(icon_darkened)
 
+var _scale_time: float = 0.05
+var _modulate_time: float = 0.1
 
 func _on_MyButt_mouse_entered():
 	# There is no pretty way to keep button centered on changing its rect
 	# https://godotengine.org/qa/10047/how-to-scale-button-when-pressed
-	assert(_tween.interpolate_property(self, "rect_scale", _scale_init, _scale_final, _tween_time, Tween.TRANS_LINEAR, Tween.EASE_OUT))
-	assert(_tween.interpolate_property(self, "rect_position", _position_init, _position_final, _tween_time, Tween.TRANS_LINEAR, Tween.EASE_OUT))
+	assert(_tween.interpolate_property(self, "rect_scale", _scale_init, _scale_final, _scale_time, Tween.TRANS_LINEAR, Tween.EASE_OUT))
+	assert(_tween.interpolate_property(self, "rect_position", _position_init, _position_final, _scale_time, Tween.TRANS_LINEAR, Tween.EASE_OUT))
 	assert(_tween.start())
-#	disable()
 
 func _on_MyButt_mouse_exited():
-	assert(_tween.interpolate_property(self, "rect_scale", _scale_final, _scale_init, _tween_time, Tween.TRANS_LINEAR, Tween.EASE_OUT))
-	assert(_tween.interpolate_property(self, "rect_position", _position_final, _position_init, _tween_time, Tween.TRANS_LINEAR, Tween.EASE_OUT))
+	assert(_tween.interpolate_property(self, "rect_scale", _scale_final, _scale_init, _scale_time, Tween.TRANS_LINEAR, Tween.EASE_OUT))
+	assert(_tween.interpolate_property(self, "rect_position", _position_final, _position_init, _scale_time, Tween.TRANS_LINEAR, Tween.EASE_OUT))
 	assert(_tween.start())
-#	enable()
 
 
 func enable():
-	disabled = false
-	_icon.modulate = Color(1, 1, 1, 1)
+	assert(_tween.interpolate_property(self,  "self_modulate", _modulate_butt_final, _modulate_init, _modulate_time, Tween.TRANS_LINEAR, Tween.EASE_OUT))
+	assert(_tween.interpolate_property(_icon, "self_modulate", _modulate_icon_final, _modulate_init, _modulate_time, Tween.TRANS_LINEAR, Tween.EASE_OUT))
+	assert(_tween.start())
 
 func disable():
-	disabled = true
-	_icon.modulate = _icon.modulate.darkened(0.5)
+	assert(_tween.interpolate_property(self,  "self_modulate", _modulate_init, _modulate_butt_final, _modulate_time, Tween.TRANS_LINEAR, Tween.EASE_OUT))
+	assert(_tween.interpolate_property(_icon, "self_modulate", _modulate_init, _modulate_icon_final, _modulate_time, Tween.TRANS_LINEAR, Tween.EASE_OUT))
+	assert(_tween.start())
