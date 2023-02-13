86e00f03e735b6b7dd6f27f723cd46cb9dc442cd
alortius <anna-alina.bondarets@ucu.edu.ua>
2023-02-13 15:36:44 +0200

    Apply real data

--- kavoons/src/scenes/enemies/Cat.gd
+++ kavoons/src/scenes/enemies/Cat.gd
@@ -38,6 +38,7 @@ var _on_hit_color: Color = Color(0.8, 0.2, 0.2, 0.8)
 ## Attach the health bar to a cat and spawn it with a random vertical offset
 func _ready():
 	_parse_cat_data()
+	_apply_cat_data()
 	
 	_hp_bar.max_value = _hp
 	_hp_bar.value = _hp_bar.max_value
@@ -70,6 +71,8 @@ func on_hit(dmg: int):
 
 
 func _reduce_health():
+	$UI/Pos/OnHover/Stats/Left/HP/CurrHP.text = str(_hp)
+	
 	assert(_tween.interpolate_property(_hp_bar, "value", _hp_bar.value, _hp, _hp_reduction_duration, Tween.TRANS_EXPO, Tween.EASE_OUT))
 	assert(_tween.start())
 
@@ -104,8 +107,19 @@ func _parse_cat_data():
 	_magical_resistance_percentage = data["magical_resistance_percentage"]
 
 
+func _apply_cat_data():
+	$UI/Pos/OnHover/Name.text = base_name
+	
+	$UI/Pos/OnHover/Stats/Left/HP/CurrHP.text = str(_hp)
+	$UI/Pos/OnHover/Stats/Left/HP/MaxHP.text = str(_hp)
+	
+	$UI/Pos/OnHover/Stats/Left/LifesCost/Label.text = str(_lifes_cost)
+	$UI/Pos/OnHover/Stats/Left/MoneyReward/Label.text = str(_money_reward)
+	
+	$UI/Pos/OnHover/Stats/Right/MoveSpeed/Label.text = str(_move_speed)
+	$UI/Pos/OnHover/Stats/Right/PhysicalArmor/Label.text = str(_physical_armor_flat)
+	$UI/Pos/OnHover/Stats/Right/MagicalResistance/Label.text = str(_magical_resistance_percentage)
 
 func _on_Area2D_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
 		_ui.visible = not _ui.visible
-
