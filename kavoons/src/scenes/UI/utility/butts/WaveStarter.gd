f1be54dcd53e6d36222f89df6743f1153a3fee76
alortius <anna-alina.bondarets@ucu.edu.ua>
2023-03-13 11:06:05 +0200

    Close on right click

--- kavoons/src/scenes/UI/utility/butts/WaveStarter.gd
+++ kavoons/src/scenes/UI/utility/butts/WaveStarter.gd
@@ -5,6 +5,9 @@ signal start_wave()
 onready var _position_init = rect_position
 onready var _position_shifted = rect_position + Vector2(0, 40)
 
+var _rotation_init := 330
+var _rotation_final := 360
+
 var is_showing = false
 var data := ""
 
@@ -18,13 +21,24 @@ func set_data(new_data):
 func show_data():
 	$Label.visible = true
 	$Label.text = data
+	assert(_tween.interpolate_property(self, "rect_rotation", _rotation_init, _rotation_final, 0.5, Tween.TRANS_ELASTIC, Tween.EASE_OUT))
+	assert(_tween.interpolate_property(self, "rect_scale", rect_scale, rect_scale, 0.5, Tween.TRANS_ELASTIC, Tween.EASE_OUT))
+	assert(_tween.start())
+
+func hide_data():
+	if not is_showing:
+		return
+	is_showing = false
+	assert(_tween.interpolate_property(self, "rect_rotation", 400, _rotation_final, 0.3, Tween.TRANS_ELASTIC, Tween.EASE_OUT))
+	assert(_tween.start())
+	$Label.visible = false
 
 func _on_WaveStarter_pressed():
 	if not is_showing:
 		is_showing = true
 		show_data()
 	else:
-		is_showing = false	
+		is_showing = false
 		emit_signal("start_wave")
 
 func clear_data():
@@ -36,3 +50,11 @@ func fade_out():
 	assert(_tween.interpolate_property(self, "modulate", Color(1, 1, 1, 1), Color(1, 1, 1, 0), 1, Tween.TRANS_EXPO, Tween.EASE_OUT))
 	assert(_tween.interpolate_property(self, "rect_position", _position_init, _position_shifted, 1, Tween.TRANS_EXPO, Tween.EASE_OUT))
 	assert(_tween.start())
+
+
+func _on_WaveStarter_gui_input(event):
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == BUTTON_LEFT:
+			_on_WaveStarter_pressed()
+		if event.button_index == BUTTON_RIGHT:
+			hide_data()
