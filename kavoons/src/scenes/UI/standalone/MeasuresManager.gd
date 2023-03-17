35baf2ba5ab420a418ed422a124f9d0cb0c056e0
alortius <anna-alina.bondarets@ucu.edu.ua>
2023-03-17 16:54:30 +0200

    Draw starters on top of cats bars

--- kavoons/src/scenes/UI/standalone/MeasuresManager.gd
+++ kavoons/src/scenes/UI/standalone/MeasuresManager.gd
@@ -7,7 +7,7 @@ onready var _economics: HBoxContainer = $Economy
 onready var _lifes: HBoxContainer = $Lifes
 
 func set_init_money(money: int):
-	_economics._update_money(money)
+	_economics.money_total = money
 
 func set_init_lifes(lifes: int):
-	_lifes._update_lifes(lifes)
+	_lifes._lifes_remain = lifes
