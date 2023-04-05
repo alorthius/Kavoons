09d87f68617895a4304ff5aff1247edfe16e998c
alortius <anna-alina.bondarets@ucu.edu.ua>
2023-04-05 13:03:41 +0300

    Animate label text

--- kavoons/src/scenes/UI/utility/HoverIcon.gd
+++ kavoons/src/scenes/UI/utility/HoverIcon.gd
@@ -1,19 +1,23 @@
 extends Control
 
 onready var _shader = $Icon.material
+onready var _label = $Label
 onready var _tween = $Tween
-
 
 func _ready():
 	_shader.set_shader_param("width", 0)
+	_label.percent_visible = 0
+
+func set_label(text):
+	_label.text = str(text)
 
 
 func _on_Icon_mouse_entered():
-#	_shader.set_shader_param("width", 1)
-	assert(_tween.interpolate_property(_shader, "shader_param/width", 0, 1, 0.1))
+	assert(_tween.interpolate_property(_shader, "shader_param/width", 0, 1, 0.1, Tween.TRANS_QUAD, Tween.EASE_IN_OUT))
+	assert(_tween.interpolate_property(_label, "percent_visible", 0, 1, 0.1, Tween.TRANS_QUAD, Tween.EASE_IN_OUT))
 	assert(_tween.start())
 
-
 func _on_Icon_mouse_exited():
-	assert(_tween.interpolate_property(_shader, "shader_param/width", 1, 0, 0.1))
+	assert(_tween.interpolate_property(_shader, "shader_param/width", 1, 0, 0.1, Tween.TRANS_QUAD, Tween.EASE_OUT_IN))
+	assert(_tween.interpolate_property(_label, "percent_visible", 1, 0, 0.1, Tween.TRANS_QUAD, Tween.EASE_OUT_IN))
 	assert(_tween.start())
